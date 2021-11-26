79ab0b9ae3398aca1146d7fe9a48fa0bceff86b2
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2021-11-26 11:18:57 -0500

    Publicado desde AndroidStudio para la clase de Fragmentos en el Curso de Desarrollo de App para Android usando Kotlin, Univesidad de Antioquia y minTIC

---
+++ app/src/main/java/co/gov/mintic/misiontic/udea/ejemplofragmentos/FragmentA.kt
@@ -0,0 +1,69 @@
+package co.gov.mintic.misiontic.udea.ciclo4
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import co.gov.mintic.misiontic.udea.ejemplofragmentos.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentA : Fragment() {
+
+    private lateinit var message: TextView
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            count = it.getInt(ARG_COUNT, 0)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_clear).setOnClickListener { clear() }
+        view.findViewById<Button>(R.id.btn_count).setOnClickListener { add() }
+
+        message = view.findViewById(R.id.text_message)
+        updateUI()
+
+    }
+
+    private fun add() {
+        count++
+        updateUI()
+    }
+
+    private fun clear() {
+        count = 0
+        updateUI()
+    }
+
+    private fun updateUI() {
+        message.text = requireContext().getString(R.string.text_message, count)
+    }
+
+    companion object {
+        const val ARG_COUNT = "count"
+
+        fun newInstance(count: Int): FragmentA =
+            FragmentA().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COUNT, count)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/co/gov/mintic/misiontic/udea/ejemplofragmentos/MainActivity.kt
@@ -0,0 +1,36 @@
+package co.gov.mintic.misiontic.udea.ejemplofragmentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.gov.mintic.misiontic.udea.ejemplofragmentos.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        launchFragment()
+    }
+
+    private fun launchFragment() {
+        // Create new fragment and transaction
+        val fragmentA = FragmentA.newInstance(146)
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.apply {
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack
+            replace(R.id.fragment_container, fragmentA, TAG)
+            // Commit the transaction
+            commit()
+        }
+
+        // Getting fragment dynamically
+//         val fragment = supportFragmentManager.findFragmentByTag(TAG)
+    }
+
+    companion object {
+        const val TAG = "TAG_FRAGMENT_A"
+    }
+}
